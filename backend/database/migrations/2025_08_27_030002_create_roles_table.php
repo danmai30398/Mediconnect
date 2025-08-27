b996ad6fdf31a1a360edafbc8e0b3583a2cf6616
Mai Dan <danmai699@gmail.com>
2025-08-28 02:13:39 +0700

    updated

--- backend/database/migrations/2025_08_27_030002_create_roles_table.php
+++ backend/database/migrations/2025_08_27_030002_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id('role_id');
-            $table->string('role_name');
+            $table->string('role_name'); //sửa code
         });
     }
     

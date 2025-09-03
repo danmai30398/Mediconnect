4252d021fca93ee2ec55ff5633961936de8412f8
Mai Dan <danmai699@gmail.com>
2025-09-04 00:35:58 +0700

    Update DoctorController and DoctorProfileController

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\DoctorProfileController;
+
+Route::get('/doctor/me', [DoctorController::class, 'me']);
+Route::post('/doctor/avatar', [DoctorController::class, 'uploadAvatar']);
+Route::get('/notifications', [NotificationController::class, 'index']);
+Route::post('/doctor/update', [DoctorProfileController::class, 'update']);
+
